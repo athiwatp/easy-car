368ccc6b0af83cf04b0319e7f7b235dad7a7e9dd
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-10-29 18:50:21 +0700

    Add Views

--- app.js
+++ app.js
@@ -2,4 +2,25 @@
 // https://github.com/codestar-work/easy-car
 
 // npm install express ejs mysql multer
+var express = require('express')
+var app     = express()
+var ejs     = require('ejs')
+app.listen(4000)
+app.engine('html', ejs.renderFile)
+app.get('/', showHomePage)
+app.get('/search', showSearchPage)
+app.get('/login', showLoginPage)
+app.get('/post', showPostPage)
 
+function showHomePage(req, res) {
+	res.render('index.html')
+}
+function showSearchPage(req, res) {
+	res.render('search.html')
+}
+function showLoginPage(req, res) {
+	res.render('login.html')
+}
+function showPostPage(req, res) {
+	res.render('post.html')
+}

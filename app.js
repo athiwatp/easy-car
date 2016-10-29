fa799c3e84328a7524ab006d2fa354339f6b20d9
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-10-29 19:30:55 +0700

    Add multer for post data

--- app.js
+++ app.js
@@ -5,13 +5,21 @@
 var express = require('express')
 var app     = express()
 var ejs     = require('ejs')
+var multer  = require('multer')
+var uploader= multer({dest: 'uploads/'})
 app.listen(4000)
 app.engine('html', ejs.renderFile)
 app.get('/', showHomePage)
 app.get('/search', showSearchPage)
 app.get('/login', showLoginPage)
 app.get('/post', showPostPage)
+app.post('/login', uploader.single(), checkLogin)
 app.use( express.static('public') )
+
+function checkLogin(req, res) {
+	// req.body.user
+	// req.body.password
+}
 
 function showHomePage(req, res) {
 	res.render('index.html')
